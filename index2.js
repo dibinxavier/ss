2122f7b93f32bb7e7d4f03393f8db5a3136552dc
xavierdibin <72697894+xavierdibin@users.noreply.github.com>
2020-10-11 19:42:57 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -742,13 +742,13 @@ socket.on('receivedAck', function (data) {
             console.log("3rd acknwoledge received : ");
         })
 
-
-      //changes to remove
    
     socket.on('sendAck', function (data) {
         var socketelement=true;
         var socketdata=false;
     console.log("message send : ");
+
+      //changes to remove
 })
 
   
