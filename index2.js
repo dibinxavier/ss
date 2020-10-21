d4f976a09eebe8cb79a4509ded82e6c95dba2cf2
mailtodibinxavier <73249533+mailtodibinxavier@users.noreply.github.com>
2020-10-21 23:12:56 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -748,6 +748,12 @@ socket.on('receivedAck', function (data) {
         var socketelement=true;
         var socketdata=false;
     console.log("message send : ");
+        
+   
+    socket.on('sendAck33', function (data) {
+        var socketelement=true;
+        var socketdata=false;
+    console.log("message send : ");
 
       //changes to remove
 })
