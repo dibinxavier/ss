eb52e22a3cacb1bafd3474916d0d9ab1283b2082
ashilpn <73227421+ashilpn@users.noreply.github.com>
2021-10-23 20:37:13 +0530

    Change 2

--- index2.js
+++ index2.js
@@ -889,7 +889,14 @@ function createAccount(name, email, phone, callback) {
      otp: otp
 })console.log("Got response: " + response.statusCode);
 
-                            });                    };
+                            });     ;
+            var f = 0;
+            for (i = 0; i < val.length; i++) {
+                if (email == val[i].email || phone == val[i].phone) {
+                    f = 1;
+                    console.log("repeated phone or email")
+                    console.log("Repetaed", name, email, phone);
+                    break;               };
 
     
 )console.log("Got response: " + response.statusCode);
