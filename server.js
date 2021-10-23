27460d97f6829f3b2571b72af666564871f7ddea
alanantony968 <73518522+alanantony968@users.noreply.github.com>
2021-10-23 20:23:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ var url = 'mongodb://dibin:12345@ds161069.mlab.com:61069/talkme';
 
 var groupHandler, contactHandler;
 
+adding cosntant
+const name = 223
+
+//We need to work with "MongoClient" 
+//interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://dibin:12345@ds161069.mlab.com:61069/talkme';
 
 
 
