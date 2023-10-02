563809def821eb06a6d7e558e6b6b0304cbd67e2
dcxphotos <56997541+dcxphotos@users.noreply.github.com>
2023-10-02 15:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ function base64_encode(file) {
     var bitmap = fs.readFileSync(file);
     // convert binary data to base64 encoded string
     return new Buffer(bitmap).toString('base64');
+}// function to encode file data to base64 encoded string
+function base64_decode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
 }
 
 
